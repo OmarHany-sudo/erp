666addc406084b8bccae07084c05af61e796d7dd
Omar hany shalaby <S23511739EG@Std.aou.edu.eg>
2025-09-13 06:32:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(bodyParser.json());
 const path = require("path");
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 // Error handling middleware
